1e25b06537b0bf93bac7f91733e1633af323cb76
Jeremy Meharg <jeremymm125@gmail.com>
2022-06-15 19:40:50 -0700

    Update Questions.js
    fixed the file and updated the var tag to let

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,26 +1,26 @@
 import '../styles/Home.css'
 import React from 'react';
 
-var currentEmotion = '';
-var desiredEmotion = '';
+let currentEmotion = '';
+let desiredEmotion = '';
 
 class Questions extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             submit: false,
-            feeling: '',
-            desiredFeeling: '',
+            // feeling: '',
+            // desiredFeeling: '',
         };
     }
 
     onFSubmit = event => {
         this.setState({submit: !this.state.submit})//sets it to opposite of previous value
-        this.setState({feeling: currentEmotion})
+        // this.setState({feeling: currentEmotion})
     }
 
     onLSubmit = event => {
-        this.setState({desiredFeeling: desiredEmotion})
+        // this.setState({desiredFeeling: desiredEmotion})
     }
 
     render() {
@@ -29,20 +29,16 @@ class Questions extends React.Component {
                 <>
                     <h3 className='question'>HOW DO YOU WANT TO FEEL?</h3>
                     <p>This is where the user input will go!</p>
-                    <button onClick={this.onLSumbit.bind(this)}>SUBMIT</button>
+                    <button onClick={this.onLSubmit.bind(this)}>SUBMIT</button>
                 </>
             :
                 <>
                     <h3 className='question'>HOW ARE YOU FEELING?</h3>
                     <p>This is where the user input will go!</p>
-                    <button onClick={this.onFSumbit.bind(this)}>SUBMIT</button>
+                    <button onClick={this.onFSubmit.bind(this)}>SUBMIT</button>
                 </>
         )
     }
 }
 
 export default Questions;
-
-{/* <h3 className='question'>HOW ARE YOU FEELING? </h3>
-<p>This is where the user input will go!</p>
-<p>SUBMIT</p> */}
