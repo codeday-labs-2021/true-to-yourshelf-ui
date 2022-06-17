c90020c1d4d0152d35922876f445b888055be08f
Jeremy Meharg <jeremymm125@gmail.com>
2022-06-17 14:04:58 -0700

    last commit

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -9,18 +9,32 @@ class Questions extends React.Component {
         super(props);
         this.state = {
             submit: false,
-            // feeling: '',
-            // desiredFeeling: '',
+            feeling: '',
+            desiredFeeling: '',
         };
     }
 
     onFSubmit = event => {
-        this.setState({submit: !this.state.submit})//sets it to opposite of previous value
-        // this.setState({feeling: currentEmotion})
+        if (document.getElementById("current").value === ''){
+            document.getElementById("current").placeholder = 'Enter Emotion';
+        }
+        else{
+            currentEmotion =  document.getElementById("current").value
+            this.setState({feeling: currentEmotion})
+            document.getElementById("current").value = '';
+            document.getElementById("current").placeholder = '';
+            this.setState({submit: !this.state.submit})//sets it to opposite of previous value
+        }
     }
 
     onLSubmit = event => {
-        // this.setState({desiredFeeling: desiredEmotion})
+        if (document.getElementById("desired").value === ''){
+            document.getElementById("desired").placeholder = 'Enter Emotion';
+        }
+        else{
+            desiredEmotion =  document.getElementById("desired").value
+            this.setState({desiredFeeling: desiredEmotion})
+        }
     }
 
     render() {
@@ -28,13 +42,13 @@ class Questions extends React.Component {
             this.state.submit ?
                 <>
                     <h3 className='question'>HOW DO YOU WANT TO FEEL?</h3>
-                    <p>This is where the user input will go!</p>
+                    <input type="text" id="desired"/><br/>
                     <button onClick={this.onLSubmit.bind(this)}>SUBMIT</button>
                 </>
             :
                 <>
                     <h3 className='question'>HOW ARE YOU FEELING?</h3>
-                    <p>This is where the user input will go!</p>
+                    <input type="text" id="current"/><br/>
                     <button onClick={this.onFSubmit.bind(this)}>SUBMIT</button>
                 </>
         )
