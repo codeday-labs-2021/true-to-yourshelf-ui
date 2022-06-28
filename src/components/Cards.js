ad9ac26fbe62db4cb983887adead6a2115a45176
Jeremy Meharg <jeremymm125@gmail.com>
2022-06-28 10:40:54 -0700

    changes for data

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 import {Card} from 'react-bootstrap';
 import TrueToYourShelfApi from '../api/TrueToYourShelfApi';
 import { createSearchParams } from 'react-router-dom';
+import { findByTitle } from '@testing-library/react';
 // import { render } from 'react-dom';
 
 class Cards extends React.Component {
@@ -18,47 +19,57 @@ class Cards extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {
-            books: [],
-        }
     }
 
-    organizeBooks(){
-        let {cardInfo} = this.props;
-        const newBooks = cardInfo.map(book => {
-            return {
-                title: book["sourceResource.title"],
-                author: book["sourceResource.creator"],
-                description: book["sourceResource.description"],
-                object: book.object,
-            }
-        })
+    // mapBooksToCard(book){
+        
+    //     return(
+    //             title.setBookTitle(book["sourceResource.title"]),
+    //             // author: book["sourceResource.creator"],
+    //             // description: book["sourceResource.description"],
+    //             // object: book.object,
+    //     )
 
-        const newState = Object.assign({}, this.state, {
-            books: newBooks
-        });
+    //     const newState = Object.assign({}, this.state, {
+    //         books: newBooks
+    //     });
 
-        this.setState(newState);
-    }
+    //     this.setState(newState);
+    //     <Card style={{ width: "18rem" }} className="box">  
+    //             <Card.Img variant="top" src={image}/>
+    //             <Card.Body>
+    //                 <Card.Title>{this.state.books.get("title")}</Card.Title>
+    //                 <Card.Text>{this.state.books.get("author")}</Card.Text>
+    //                 <Card.Text>{this.state.books.get("description")}</Card.Text>
+    //             </Card.Body>
+    //             </Card>
+    // }
 
     renderCards(){
-        console.log("book 0 " + this.state.books[0]);
+        const {cardInfo} = this.props;
+        console.log("book " + cardInfo);
         let image = "";
         let defaultImage = "https://islandpress.org/sites/default/files/default_book_cover_2015.jpg";
-        this.state.books.forEach(element => {
-            if(!element.has("object")){
-                image = defaultImage;
-            }
-            else{
-                image = this.state.books.get("object")
-            }
+        cardInfo.map(element => {
+            console.log(element);
+            let title = element["sourceResource.title"];
+            let author = element["sourceResource.creator"];
+            let description = element["sourceResource.description"];
+            let object = element.object;
+            console.log(description);
+            // if(!element.has("object")){
+            //     image = defaultImage;
+            // }
+            // else{
+            //     image = cardInfo.get("object")
+            // }
             return(
                 <Card style={{ width: "18rem" }} className="box">  
                 <Card.Img variant="top" src={image}/>
                 <Card.Body>
-                    <Card.Title>{this.state.books.get("title")}</Card.Title>
-                    <Card.Text>{this.state.books.get("author")}</Card.Text>
-                    <Card.Text>{this.state.books.get("description")}</Card.Text>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>{author}</Card.Text>
+                    <Card.Text>{description}</Card.Text>
                 </Card.Body>
                 </Card>
             );
